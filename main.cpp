26780c7dd731b6640b40ced23c1c2b18c3a2e994
Блудилин Антон <mashinist.2004@mail.ru>
2025-03-12 16:17:13 +0300

    первичный коммит

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int a, b;
+
+    cout << "Enter A: ";
+    cin >> a;
+    cout << endl << "Enter B: ";
+    cin >> b;
+    cout << endl;
+
+    return 0;
+}
